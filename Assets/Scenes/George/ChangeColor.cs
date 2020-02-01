d19af990551a8996cedd1c580e2658dfaa23addb
George Mavroeidis <gdmavroeidis@hotmail.com>
2020-02-01 00:07:31 -0500

    george health

---
+++ Assets/Scenes/George/ChangeColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public int health;
+
+    void Start() {
+        health = 100;
+    }
+
+    void Update() {
+       // t += Time.deltaTime/duration;
+       // this.GetComponent<SpriteRenderer>().material.color = Color.Lerp(startColor, endColor, t);
+        if (health < 0) {
+            health = 0;
+        }
+    }
+
+    void OnMouseOver() {
+        if (Input.GetMouseButtonDown(0)) {
+            health -= 10;
+        }
+    }
+}
---
+++ Assets/Scenes/George/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public GameObject[] rooms = new GameObject[5];
+    public int maxHealth = 100;
+    public int currentHealth;
+
+
+    private Transform bar;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {   
+        currentHealth = 0;
+        for (int i = 0; i < rooms.Length; i++) {
+            currentHealth += rooms[i].GetComponent<ChangeColor>().health;
+        }
+        bar.transform.localScale = new Vector2(currentHealth/100/0f, 1);
+    }   
+}
