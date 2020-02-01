e391cfc0a5003737d8912a6f6dbeb0b7f54b8c91
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2020-02-01 00:31:09 -0500

    lol

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,22 +5,55 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float Speed;
-    public float Acceleration;
+    public bool IsClimbing=false;
+    private Rigidbody2D RB;
 
-    public float CurrentSpeed;
-    public float TargetSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        RB = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        TargetSpeed = Input.GetAxisRaw("Horizontal") * Speed;
-         
-        
+        MoveCharacter();
     }
+
+    public void MoveCharacter()
+    {
+
+        float UserInputHorizontal=Input.GetAxisRaw("Horizontal");
+        float UserInputVertical;
+        if (IsClimbing)
+        {
+            UserInputVertical = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            UserInputVertical = 0;
+        }
+        transform.position = new Vector3(Time.deltaTime*Speed*UserInputHorizontal+transform.position.x, Time.deltaTime * Speed * UserInputVertical + transform.position.y, 0);
+    }
+
+    public void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "Ladder")
+        {
+            IsClimbing = true;
+            RB.gravityScale = 0;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Ladder")
+        {
+            IsClimbing = false;
+            RB.gravityScale = 1;
+
+        }
+    }
+
 }
