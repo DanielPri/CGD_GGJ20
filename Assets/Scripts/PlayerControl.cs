775b054d05194bae1378db55d9a01e9acc25119f
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2020-01-31 23:16:38 -0500

    bla

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float Speed;
+    public float Acceleration;
+
+    public float CurrentSpeed;
+    public float TargetSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TargetSpeed = Input.GetAxisRaw("Horizontal") * Speed;
+         
+        
+    }
+}
