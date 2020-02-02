6bbffe371e93e36646a3d176c7d2dfc791791039
Letícia Pasdiora <leticiapasdiora@gmail.com>
2020-02-01 20:31:49 -0500

    created the basic logic to change scenes and fixed a small bug

---
+++ Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+/**************************************************
+ * Script to handle the buttons in the main menu
+ * Mainly methods to load the scenes
+ * and one to show/hide the credits
+ *************************************************/
+
+public class MainMenuHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject _creditsPanel = null;
+    [SerializeField] private GameObject _mainMenuComponents = null;
+
+    public void ShowCredits()
+    {
+        if (_creditsPanel != null && _mainMenuComponents != null)
+        {
+            _mainMenuComponents.SetActive(false);
+            _creditsPanel.SetActive(true);
+        }
+    }
+
+
+    public void HideCredits()
+    {
+        if (_creditsPanel != null && _mainMenuComponents != null)
+        {
+            _creditsPanel.SetActive(false);
+            _mainMenuComponents.SetActive(true);
+        }
+    }
+
+    public void LoadGameScene()
+    {
+        // TODO: Change the name to the correct scene
+        // NOT WORKING!
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadInstructionsScene()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void LoadHighScoresScene()
+    {
+        SceneManager.LoadScene("ScoreBoard");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# CGD_GGJ20
+# Star Ship Frenzy
+
+## Galaxy shader
+Star Nest algorithm by Pablo Román Andrioli
+
+Unity 5.x shader by Jonathan Cohen
+
+This content is under the MIT License.
+
+
+Original Shader:
+https://www.shadertoy.com/view/XlfGRj
