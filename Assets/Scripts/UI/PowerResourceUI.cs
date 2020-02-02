3327013ec9d587bc33f729eda044ec622311b8bc
DanielPri <privorotskyd@gmail.com>
2020-02-02 03:02:03 -0500

    cha cha real smooth. Also UI stuff

---
+++ Assets/Scripts/UI/PowerResourceUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerResourceUI : MonoBehaviour
+{
+    Text powerText;
+    GameObject powerUI;
+    PowerPlantRoom powerPlantRoom;
+
+    public RepairZone powerRoom;
+
+    void Start()
+    {
+        powerUI = transform.GetChild(0).gameObject;
+        powerText = transform.GetChild(0).GetComponentInChildren<Text>();
+        if (powerPlantRoom == null) { powerPlantRoom = GameObject.Find("PowerPlantRoom").GetComponent<PowerPlantRoom>(); }
+        if (powerRoom == null) { powerRoom = GameObject.Find("PowerPlantRoom").GetComponentInChildren<RepairZone>(); }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        powerText.text = powerPlantRoom.currentEnergy.ToString();
+        handleVisibility();
+    }
+    private void handleVisibility()
+    {
+        powerUI.SetActive(powerRoom.isOccupied);
+    }
+}
