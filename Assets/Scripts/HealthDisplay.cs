ca1fd547c29de381e12340644550f474a5ec1968
Patrick Pin <patrickpin840@hotmail.com>
2020-02-02 15:42:14 -0500

    Health bugfix

--- Assets/Scripts/HealthDisplay.cs
+++ Assets/Scripts/HealthDisplay.cs
@@ -7,16 +7,16 @@ public class HealthDisplay : MonoBehaviour
     [SerializeField] GameController controller;
     [SerializeField] Sprite green;
     [SerializeField] Sprite red;
-    public Sprite[] healthBar = new Sprite[4];
+    SpriteRenderer[] healthBar = new SpriteRenderer[4];
     public int counter;
     private int index;
 
     private void Start()
     {
-        healthBar[0] = transform.GetChild(0).GetComponent<SpriteRenderer>().sprite;
-        healthBar[1] = transform.GetChild(1).GetComponent<SpriteRenderer>().sprite;
-        healthBar[2] = transform.GetChild(2).GetComponent<SpriteRenderer>().sprite;
-        healthBar[3] = transform.GetChild(3).GetComponent<SpriteRenderer>().sprite;
+        healthBar[0] = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        healthBar[1] = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        healthBar[2] = transform.GetChild(2).GetComponent<SpriteRenderer>();
+        healthBar[3] = transform.GetChild(3).GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -29,28 +29,28 @@ public class HealthDisplay : MonoBehaviour
         switch (counter)
         {
             case 0:
-                healthBar[0] = green;
-                healthBar[1] = green;
-                healthBar[2] = green;
-                healthBar[3] = green;
+                healthBar[0].sprite = green;
+                healthBar[1].sprite = green;
+                healthBar[2].sprite = green;
+                healthBar[3].sprite = green;
                 break;
             case 1:
-                healthBar[0] = green;
-                healthBar[1] = green;
-                healthBar[2] = green;
-                healthBar[3] = red;
+                healthBar[0].sprite = green;
+                healthBar[1].sprite = green;
+                healthBar[2].sprite = green;
+                healthBar[3].sprite = red;
                 break;
             case 2:
-                healthBar[0] = green;
-                healthBar[1] = green;
-                healthBar[2] = red;
-                healthBar[3] = red;
+                healthBar[0].sprite = green;
+                healthBar[1].sprite = green;
+                healthBar[2].sprite = red;
+                healthBar[3].sprite = red;
                 break;
             case 3:
-                healthBar[0] = green;
-                healthBar[1] = red;
-                healthBar[2] = red;
-                healthBar[3] = red;
+                healthBar[0].sprite = green;
+                healthBar[1].sprite = red;
+                healthBar[2].sprite = red;
+                healthBar[3].sprite = red;
                 break;
 
         }
